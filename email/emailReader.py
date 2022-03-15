4c7b4ef11aa2e48281cc2328a9cb69f17c601b31
Your Name <arafey183@gmail.com>
2022-03-15 19:39:53 +0500

    initial commit

--- email/emailReader.py
+++ email/emailReader.py
@@ -1,7 +1,10 @@
 import imaplib
-from typing import Dict
 import credentials
 import email
+import json
+
+from typing import Dict
+
 
 class EmailReader:
 
@@ -16,11 +19,14 @@ class EmailReader:
     
     def fetchEmail(self, emailId:int) -> Dict[str,str]:
 
-        e = self.imap.fetch(f'{emailId}', "(RFC822)")[1][0][1]
+        e = self.imap.fetch(f'{emailId+1}', "(RFC822)")[1][0][1]
         e = email.message_from_bytes(e)
         return {
-            'from'   : e['From'],
-            'to'     : e['To'],
+            '_id'    : emailId,
+            'from'   : {
+                'name'         : e['From'].split('<')[ 0].strip(),
+                'emailAddress' : e['From'].split('<')[~0][:~0]
+            },
             'subject': e['Subject'],
             'body'   : ''.join(
                 map     (lambda part: part.get_payload(decode= True).decode(),
@@ -28,4 +34,18 @@ class EmailReader:
                 e.get_payload()
             )))
         }
+    
+
+if __name__ == '__main__':
+    emailReader = EmailReader()
+    n = 10
+    emails = [
+        emailReader.fetchEmail(i)
+        for i in range(n)
+    ]
+    json.dump(
+        emails, 
+        fp= open('email/out.json', 'w'),
+        indent= 4
+    )
     
--- model/model.py
+++ model/model.py
@@ -1,16 +1,22 @@
+import re
 import flair
-import texthero
-from pandas import Series
+
+from pandas import Series, read_json
+
 
 class Model:
     
     NERtagger = flair.models.SequenceTagger.load('flair/ner-english-ontonotes-large')
 
     def clean(self, text:Series) -> Series:
-        text = texthero.remove_urls(text)
-        text = texthero.remove_punctuations(text)
+        return ( 
+            text
+            .apply(lambda s: re.sub('<.*>'   , '' , s))
+            .apply(lambda s: re.sub('http\S*', '' , s))
+            .apply(lambda s: re.sub('\s+'    , ' ', s))
+            .apply(lambda s: re.sub('=+|-+'  , '' , s))
+        )
 
-        return text
 
     
     def predict(self, emails:Series) -> Series:
@@ -21,3 +27,14 @@ class Model:
             span.to_dict()['labels'][0].to_dict()  
             for span in sentence.get_span()
         ])
+
+
+if __name__ == '__main__':
+
+    model = Model()
+
+    df = read_json('email/out.json')
+    df['cleaned'] = model.clean(df['body'])
+
+    df.to_json('model/out.json', orient= 'records', indent= 4)
+    pass
